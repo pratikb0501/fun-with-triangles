64cdc876ea73dafa79a626975aa4eb960dc2d34f
pratikb0501 <pratikbagmare@gmail.com>
2021-09-23 23:09:22 +0530

    added js

---
+++ index.js
@@ -0,0 +1,17 @@
+const formDataArray = document.getElementById("quiz-form");
+const submitBtn = document.getElementById("content-btn")
+const message = document.getElementById("message");
+
+
+const answers = ["yes", "right", "equilateral", "5", "scalene"]
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    // let formData = new FormData(formDataArray)
+    // console.log(formData.values)
+    // for (let value in formData.values()) {
+    //     console.log(value)
+    // }
+
+
+})
